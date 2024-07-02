835bcbb5ff73ab87376258de733ff7a9db18a40d
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-07-02 18:31:19 +0530

    origin added using hook :zap:

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import {
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
@@ -42,6 +43,7 @@ const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
   const [loading, setLoading] = useState<boolean>(false);
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
   const { storeId } = params;
 
   const form = useForm<SettingsFormValues>({
