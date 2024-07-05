a40831f0bbfd5472a1848a75b4d8564c70f74ebd
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-07-05 16:06:48 +0530

    Billboard API Implemented :rainbow:

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
@@ -4,17 +4,29 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { PlusCircle } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import React from "react";
+import React, { FC, useEffect } from "react";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import Image from "next/image";
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+const BillboardClient: FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   const { storeId } = params;
+
+  useEffect(() => {
+    router.refresh();
+  }, []);
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -26,6 +38,23 @@ const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      {data.length ? (
+        <DataTable columns={columns} data={data} searchKey="label" />
+      ) : (
+        <div className="flex flex-col items-center justify-center h-[50vh]">
+          <Image
+            src={require("../../../../../../public/empty.png")}
+            alt="empty"
+            width={250}
+            height={250}
+          />
+          <h3 className="text-center tracking-tighter w-full text-slate-500 text-2xl">
+            No billboards found
+            <br />
+            Please create new one.
+          </h3>
+        </div>
+      )}
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import BillboardClient from "./components/billboard-client";
+import db from "@/lib/db";
+import { BillboardColumn } from "./components/columns";
+import {format} from "date-fns"
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboard = await db.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboard.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMM do, yyyy")
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
